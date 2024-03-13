6cd52da51d6605fc77b5705bd7768aa608136666
trevor@utilityapi.com <trevor@utilityapi.com>
2024-03-12 20:15:22 -0600

    Switch to light mode B-)

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,12 @@
 function ColorMyPencils(color)
+    if vim.fn.has('termguicolors') == 1 then
+        vim.opt.termguicolors = true
+    end
 	color = color or "everforest"
 	vim.cmd.colorscheme(color)
-        vim.o.background = "dark"
+        vim.o.background = "light"
+    vim.g.everforest_background = 'soft'
+    vim.g.everforest_better_performance = 1
 end
 
 ColorMyPencils()
