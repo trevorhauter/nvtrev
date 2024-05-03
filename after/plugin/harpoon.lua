bb4f97c9d4a423d6c9768c5e6a436a400d9ebe2d
trevor@utilityapi.com <trevor@utilityapi.com>
2024-05-03 10:55:35 -0400

    Remove deprecated call

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,6 +1,6 @@
 local harpoon = require("harpoon")
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-h>", function() harpoon:list():clear() end)
 vim.keymap.set("n", "<S-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
