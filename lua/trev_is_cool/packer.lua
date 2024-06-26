82b68ea9835f645ed1ad12e003d1afb390eec6a9
trevor@utilityapi.com <trevor@utilityapi.com>
2024-06-26 10:37:08 -0400

    Alphabetize packer config

--- lua/trev_is_cool/packer.lua
+++ lua/trev_is_cool/packer.lua
@@ -4,35 +4,21 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
- 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  -- Ale - for linting!
+  use('dense-analysis/ale')
+  
 
-  use {
-      'nvim-tree/nvim-tree.lua',
-      requires = {
-          'nvim-tree/nvim-web-devicons', -- optional
-      },
-  }
-
-  --use {
-      --'sainnhe/gruvbox-material', config = function() vim.cmd.colorscheme("gruvbox-material") end 
-  --}
+  -- everforest - a beautiful theme!
   use {
     'sainnhe/everforest', config = function() vim.cmd.colorscheme('everforest') end
   }
-  -- For syntax highlighting
-  -- At the moment, it doesn't seem like TSUpdate is auto-running. So if you run into issues with treesitter,
-  -- run that command
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
 
-  -- To easily jump between files
+
+  -- Fugitive - git integration 
+  use('tpope/vim-fugitive')
+
+
+  -- Harpoon (and dependencies) - To easily jump between files
   use "nvim-lua/plenary.nvim" 
   use {
       "ThePrimeagen/harpoon",
@@ -40,31 +26,12 @@ return require('packer').startup(function(use)
       requires = { {"nvim-lua/plenary.nvim"} }
   }
 
-  -- To keep track of changes made to files
-  use('mbbill/undotree')
 
-  -- Git configuration
-  use('tpope/vim-fugitive')
-
-  -- To easily comment/uncomment lines
-  use('preservim/nerdcommenter')
-
-  -- Adds some flare to the bottom of the screen with extra info
-  use('vim-airline/vim-airline')
-
-  -- Real time git feedback when editing files
-  use('airblade/vim-gitgutter')
-
-  -- To add some context to where you are in your file
-  use('simrat39/symbols-outline.nvim') 
-  
-  -- Linting!
-  use('dense-analysis/ale')
-  
-  -- sorting python imports
+  -- Isort - sorting python imports
   use('stsewd/isort.nvim')
 
-  -- LSP support. The big one!
+
+  -- lsp-zero - LSP support. The big one!
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v3.x',
@@ -80,5 +47,54 @@ return require('packer').startup(function(use)
 		  {'L3MON4D3/LuaSnip'},
 	  }
   }
+
+
+  -- nerdcommenter - To easily comment/uncomment lines
+  use('preservim/nerdcommenter')
+
+
+  -- nvim-tree - file tree navigation!
+  use {
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+          'nvim-tree/nvim-web-devicons', -- optional
+      },
+  }
+
+
+  -- nvim-treesitter (and dependencies) - For syntax highlighting
+  -- At the moment, it doesn't seem like TSUpdate is auto-running. So if you run into issues with treesitter,
+  -- run that command
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+
+
+  -- Packer - it can manage itself
+  use 'wbthomason/packer.nvim'
+
+
+  -- symbols-outline - To add some context to where you are in your file (might remove this...)
+  use('simrat39/symbols-outline.nvim') 
+
+
+  -- telescope - to help us find what we need anywhere in our project
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+
+  -- undotree - To keep track of changes made to files
+  use('mbbill/undotree')
+
+
+  -- vim-airling - Adds some flare to the bottom of the screen with extra info
+  use('vim-airline/vim-airline')
+
+
+  -- vim-gitugtter - Real time git feedback when editing files
+  use('airblade/vim-gitgutter')
+
 
  end) 
