c96ee438cde11dc7f8595d2e075494d112bfd053
trevor@utilityapi.com <trevor@utilityapi.com>
2024-06-26 12:12:35 -0400

    Update colors for quickscope to be in line with colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,6 +3,17 @@ function ColorMyPencils(color)
         vim.opt.termguicolors = true
     end
 	color = color or "everforest"
+
+    -- Define QuickScope highlights
+    -- This updates our highlight colors to be in line with our color scheme automatically!
+    vim.cmd([[
+        augroup qs_colors
+          autocmd!
+          autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+          autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+        augroup END
+    ]])
+
 	vim.cmd.colorscheme(color)
         vim.o.background = "dark"
     vim.g.everforest_background = 'soft'
