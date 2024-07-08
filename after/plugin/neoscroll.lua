ccf5336667b766b70089485d67011ba73ebd8ffc
Trevor <trevor@utilityapi.com>
2024-07-08 15:52:08 -0400

    Adjust scroll settings

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -9,7 +9,7 @@ require('neoscroll').setup({
   stop_eof = true,             -- Stop at <EOF> when scrolling downwards
   respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
   cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  easing = 'quadratic',           -- Default easing function
+  easing = 'linear',           -- Default easing function
   pre_hook = nil,              -- Function to run before the scrolling animation starts
   post_hook = nil,             -- Function to run after the scrolling animation ends
   performance_mode = false,    -- Disable "Performance Mode" on all buffers.
